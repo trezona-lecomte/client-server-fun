61acdeadfb7e40e1e7f0834c9b1f4658603ff60d
trezona-lecomte <trezona.lecomte@gmail.com>
2015-01-26 22:46:58 +1300

    Add DB interface

---
+++ src/suncertify/db/DuplicateKeyException.java
@@ -0,0 +1,34 @@
+package suncertify.db;
+
+/**
+ *
+ * @author 91023151
+ */
+public class DuplicateKeyException extends Exception {
+
+	private static final long serialVersionUID = 4895806584647173815L;
+	private String message;
+
+	/**
+	 * Creates a new DuplicateKeyException with <code>null<code> as its
+	 * message.
+	 */
+	public DuplicateKeyException() {
+	};
+
+	/**
+	 * Creates an instance of a DuplicateKeyException with the specified
+	 * message.
+	 */
+	public DuplicateKeyException(String message) {
+		this.message = message;
+	};
+
+	/**
+	 * Returns the detail message string of this DuplicateKeyException.
+	 */
+	@Override
+	public String getMessage() {
+		return message;
+	};
+}
---
+++ src/suncertify/db/RecordNotFoundException.java
@@ -0,0 +1,35 @@
+package suncertify.db;
+
+/**
+*
+* @author 91023151
+*/
+public class RecordNotFoundException extends Exception {
+
+    private static final long serialVersionUID = 2823045322464184130L;
+	private String message;
+
+	/**
+	 * Creates a new RecordNotFoundException with <code>null<code> as its
+	 * message.
+	 */
+	public RecordNotFoundException() {
+	};
+
+	/**
+	 * Creates an instance of a RecordNotFoundException with the specified
+	 * message.
+	 */
+	public RecordNotFoundException(String message) {
+	    this.message = message;
+	};
+
+	/**
+	 * Returns the detail message string of this RecordNotFoundException.
+	 */
+	@Override
+	public String getMessage() {
+	    return message;
+	};
+
+}
