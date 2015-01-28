23ce713239d183c3a955e1171257f5eaaf4206ce
trezona-lecomte <trezona.lecomte@gmail.com>
2015-01-28 18:44:03 +1300

    Add ContractorDatabase class

---
+++ src/test/suncertify/db/ContractorDatabaseTest.java
@@ -0,0 +1,55 @@
+package test.suncertify.db;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class ContractorDatabaseTest {
+
+    @Before
+    public void setUp() throws Exception {
+
+    }
+
+    @After
+    public void tearDown() throws Exception {
+
+    }
+
+    @Test
+    public void testRead() throws Exception {
+
+    }
+
+    @Test
+    public void testUpdate() throws Exception {
+
+    }
+
+    @Test
+    public void testDelete() throws Exception {
+
+    }
+
+    @Test
+    public void testFind() throws Exception {
+
+    }
+
+    @Test
+    public void testCreate() throws Exception {
+
+    }
+
+    @Test
+    public void testLock() throws Exception {
+
+    }
+
+    @Test
+    public void testUnlock() throws Exception {
+
+    }
+}
