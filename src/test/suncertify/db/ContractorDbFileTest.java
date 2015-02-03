590bac61db9548eda38340766b8da6adfc42e350
trezona-lecomte <trezona.lecomte@gmail.com>
2015-02-03 22:37:01 +1300

    Add test skeleton for db adapter class

---
+++ src/test/suncertify/db/ContractorDbFileTest.java
@@ -0,0 +1,18 @@
+package test.suncertify.db;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class ContractorDbFileTest {
+
+    @Test
+    public void testGetContractors() throws Exception {
+
+    }
+
+    @Test
+    public void testGetContractor() throws Exception {
+
+    }
+}
