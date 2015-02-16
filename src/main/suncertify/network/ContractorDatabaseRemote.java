52cde2262522acbc83a010139e59049af46c5108
trezona-lecomte <trezona.lecomte@gmail.com>
2015-02-16 19:58:49 +1300

    Add ContractorDatabaseRemote interface

---
+++ src/main/suncertify/network/ContractorDatabaseRemote.java
@@ -0,0 +1,11 @@
+package main.suncertify.network;
+
+import main.suncertify.db.DB;
+
+import java.rmi.Remote;
+
+/**
+ * Created by slimfox on 16/02/15.
+ */
+public interface ContractorDatabaseRemote extends Remote, DB {
+}
